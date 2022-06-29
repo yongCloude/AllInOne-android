f549f2e031071c416d86a26f946b77554c0a1455
yongCloude <djkye0001@naver.com>
2022-06-29 14:33:58 +0900

    [feat] 메인화면에서 회원관련 액티비티 전환 추가

--- app/src/main/java/com/hongik/pcrc/allinone/MainActivity.java
+++ app/src/main/java/com/hongik/pcrc/allinone/MainActivity.java
@@ -2,13 +2,33 @@ package com.hongik.pcrc.allinone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
+
+import com.hongik.pcrc.allinone.activity.auth.Auth;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String TAG = "MainActivity";
+
+
+    private ImageButton profile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profile = findViewById(R.id.main_profile);
+
+        profile.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), Auth.class);
+            startActivity(intent);
+        });
+
     }
+
+
 }

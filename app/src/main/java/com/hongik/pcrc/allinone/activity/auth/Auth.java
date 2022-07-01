5597c35ddd564886bf862cb5617e71560da8e9ea
yongCloude <76439014+yongCloude@users.noreply.github.com>
2022-07-01 23:15:16 +0900

    [feat] 뒤로가기 및 다른 이벤트 리스너 추가

--- app/src/main/java/com/hongik/pcrc/allinone/activity/auth/Auth.java
+++ app/src/main/java/com/hongik/pcrc/allinone/activity/auth/Auth.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.hongik.pcrc.allinone.R;
 import com.hongik.pcrc.allinone.fragment.auth.SignIn;
+import com.hongik.pcrc.allinone.fragment.auth.SignUp;
 
-public class Auth extends AppCompatActivity {
+public class Auth extends AppCompatActivity implements SignIn.Callbacks, SignUp.Callbacks {
+
+    public static String TAG = "Auth";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,7 @@ public class Auth extends AppCompatActivity {
 
         Fragment currentFragment = getSupportFragmentManager()
                 .findFragmentById(R.id.auth_fragment_container);
+
         if(currentFragment == null){
             SignIn fragment = new SignIn();
             getSupportFragmentManager()
@@ -25,5 +30,33 @@ public class Auth extends AppCompatActivity {
                     .commit();
         }
 
+    }
+
+    @Override
+    public void onSignUpSelected() {
+        Log.d(TAG,"AuthActivity.onSignUpSelected called");
+
+        SignUp signUpFragemnt = SignUp.newInstance(null, null);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.auth_fragment_container, signUpFragemnt)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    @Override
+    public void onResetPasswordSelected() {
+        Log.d(TAG,"AuthActivity.onResetPasswordSelected called");
+    }
+
+    @Override
+    public void onBackArrowSelected() {
+        Log.d(TAG,"AuthActivity.onBackArrowSelected called");
+        Fragment removeFragemnt = getSupportFragmentManager().findFragmentById(R.id.auth_fragment_container);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .remove(removeFragemnt)
+                .commit();
+        finish();
     }
 }
